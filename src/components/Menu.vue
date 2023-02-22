c5cf014900a5a059d0ce3e4cf0907412e734eb13
Samy <BrahimiSamy123@gmail.com>
2023-02-22 19:42:01 +0100

    second2edit

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -4,8 +4,24 @@
       <div class="p-4 text-lg text-center font-semibold text-gray-800">Meal Categories</div>
         <div class="flex-1 max-h-screen meal-categories overflow-y-auto">
           <div class="">
-            <button class="text-center w-full  py-1 text-gray-800 active:bg-violet-700 mb-2 focus:bg-violet-700" active>Abdou</button>
-
+            <button class="text-center w-full  py-1 text-gray-800 hover:bg-gray-400 mb-2">Breakfastaaaaa</button>
+            <button class="text-center w-full  py-1 text-gray-800 hover:bg-gray-400 mb-2">Lunch</button>
+            <button class="text-center w-full  py-1 text-gray-800 hover:bg-gray-400 mb-2">Dinner</button>
+            <button class="text-center w-full  py-1 text-gray-800 hover:bg-gray-400 mb-2">Dessert</button>
+            <button class="text-center w-full  py-1 text-gray-800 hover:bg-gray-400 mb-2">Drinks</button>
+            <button class="text-center w-full  py-1 text-gray-800 hover:bg-gray-400 mb-2">Drinks</button>
+            <button class="text-center w-full  py-1 text-gray-800 active:bg-violet-700 mb-2 focus:bg-violet-700" active></button>
+            <button class="text-center w-full  py-1 text-gray-800 active:bg-violet-700 mb-2 focus:bg-violet-700" active>BBBBBB</button>
+            <button class="text-center w-full  py-1 text-gray-800 hover:bg-gray-400 mb-2">Drinks</button>
+            <button class="text-center w-full  py-1 text-gray-800 hover:bg-gray-400 mb-2">Drinks</button>
+            <button class="text-center w-full  py-1 text-gray-800 hover:bg-gray-400 mb-2">Drinks</button>
+            <button class="text-center w-full  py-1 text-gray-800 hover:bg-gray-400 mb-2">Drinks</button>
+            <button class="text-center w-full  py-1 text-gray-800 hover:bg-gray-400 mb-2">Drinks</button>
+            <button class="text-center w-full  py-1 text-gray-800 hover:bg-gray-400 mb-2">Drinks</button>
+            <button class="text-center w-full  py-1 text-gray-800 hover:bg-gray-400 mb-2">Drinks</button>
+            <button class="text-center w-full  py-1 text-gray-800 hover:bg-gray-400 mb-2">Drinks</button>
+            <button class="text-center w-full  py-1 text-gray-800 hover:bg-gray-400 mb-2">Drinks</button>
+            <button class="text-center w-full  py-1 text-gray-800 hover:bg-gray-400 mb-2">Drinks</button>
         </div>
       </div>
     </div>
