65d76223e68bc2cede6452143f0a0cd67cb0e764
Samy <BrahimiSamy123@gmail.com>
2023-02-22 19:19:49 +0100

    sdfksdf

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -4,7 +4,7 @@
       <div class="p-4 text-lg text-center font-semibold text-gray-800">Meal Categories</div>
         <div class="flex-1 max-h-screen meal-categories overflow-y-auto">
           <div class="">
-            <button class="text-center w-full  py-1 text-gray-800 hover:bg-gray-400 mb-2">Breakfastaaaaa</button>
+            <button class="text-center w-full  py-1 text-gray-800 hover:bg-gray-400 mb-2">dsqdqsd</button>
             <button class="text-center w-full  py-1 text-gray-800 hover:bg-gray-400 mb-2">Lunch</button>
             <button class="text-center w-full  py-1 text-gray-800 hover:bg-gray-400 mb-2">Dinner</button>
             <button class="text-center w-full  py-1 text-gray-800 hover:bg-gray-400 mb-2">Dessert</button>
