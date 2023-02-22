14e7bb475d68501997223c9d0ce261312a926f7e
abdou51 <bgh.prods@gmail.com>
2023-02-22 19:53:12 +0100

    aaaa

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -4,7 +4,7 @@
       <div class="p-4 text-lg text-center font-semibold text-gray-800">Meal Categories</div>
         <div class="flex-1 max-h-screen meal-categories overflow-y-auto">
           <div class="">
-            <button class="text-center w-full  py-1 text-gray-800 active:bg-violet-700 mb-2 focus:bg-violet-700" active>Abdou</button>
+            <button class="text-center w-full  py-1 text-gray-800 active:bg-violet-700 mb-2 focus:bg-violet-700" active>Abdou A</button>
         </div>
       </div>
     </div>
